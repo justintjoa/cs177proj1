5c2dcfa162a072a1588804549f7c3f02aa06f74e
justintjoa <justintoja@gmail.com>
2020-04-12 15:06:36 -0700

    sdf

--- Minecraft/CS111/CS160/CS177/server.py
+++ Minecraft/CS111/CS160/CS177/server.py
@@ -1,57 +1,33 @@
 #Connect to cs177.seclab.cs.ucsb.edu on port 42293 to interact with your challenge!
 import socket
 import sys
+import configure_pb2
+from struct import *
 
-class my_dictionary(dict): 
-  
-    # __init__ function 
-    def __init__(self): 
-        self = dict() 
-          
-    # Function to add key:value 
-    def add(self, key, value): 
-        self[key] = value 
-  
-# Main Function 
-mydict = my_dictionary() 
+
 
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-
-
+a = configure_pb2.m1()
+a.type = 0
+a.body = "hello!"
 # Connect the socket to the port where the server is listening
 server_address = ('cs177.seclab.cs.ucsb.edu', 42293)
-print >>sys.stderr, 'connecting to %s port %s' % server_address
 sock.connect(server_address)
-print("went ok")
-length = False
-strlen = 0
-counter = 0 
-type = 0
-flag = ""
+s = a.SerializeToString()
+length = len(s)
+message=pack('H', length)
+sock.sendall(message)
+sock.sendall(s)
+print(message)
+print(s)
 while (True):
-    if (length == False):
-        strlen = sock.recv(4)
-        length = True
-    else:
-        data = proto
-        while (counter < strlen):
-            moredata = sock.recv(strlen)
-            data = data + moredata
-            counter = counter + len(moredata)
-        parse(data)
-        type = data.type
-        if (type == 1):
-            key = data.body.key
-            message = data.body.new
-            mydict.add(key,message)
-        if (type == 3):
-        if (type == 6):
-        if (type == 8):
-        if (type == 9):
-            flag = data[1]
-print(flag)
+    data = sock.recv(32)
+    print(data)
+    print('fixed')
+
+
 
             
 
