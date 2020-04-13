1f5301a4255ae451f9d837fb5711c666e860de9c
justintjoa <justintoja@gmail.com>
2020-04-12 17:47:34 -0700

    sdf

--- Minecraft/CS111/CS160/CS177/server.py
+++ Minecraft/CS111/CS160/CS177/server.py
@@ -3,30 +3,63 @@ import socket
 import sys
 import configure_pb2
 from struct import *
+from io import StringIO
 
 
 
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-a = configure_pb2.m0()
-a.type = 0
+
 # Connect the socket to the port where the server is listening
 server_address = ('cs177.seclab.cs.ucsb.edu', 42293)
 sock.connect(server_address)
-s = a.SerializeToString()
-length = (len(s))
-message=pack('H', length)
-print(message)
-print(s)
-message = message + s
-print(message)
-sock.sendall(message)
+
+request = configure_pb2.m0()
+request.type = 0
+request = request.SerializeToString()
+requestlength = (len(request))
+requestlength=pack('H', requestlength)
+request = request + requestlength
+
+setvalue = configure_pb2.m0()
+setvalue.type = 2
+setvalue = setvalue.SerializeToString()
+setvaluelength = (len(setvalue))
+setvaluelength=pack('H', setvaluelength)
+setvalue = setvalue + setvaluelength
+
+getvalue = configure_pb2.m3()
+getvalue.type = 4
+getvalue.flag = "Current value"
+
+error = configure_pb2.m0()
+error.type = 5
+error = error.SerializeToString()
+errorlength = (len(error))
+errorlength=pack('H', errorlength)
+error = error + errorlength
+
+getvalue = configure_pb2.m2()
+getvalue.type = 7
+getvalue.count = 0
+
+
+sock.sendall(request)
 
 while (True):
     data = sock.recv(32)
-    print(data)
-    print('fixed')
+    print('read')
+    command = (data[-1:])
+    if (command == b'\x01'): #setvalue
+    if (command == b'\x03'): #getvalue
+    if (command == b'\x06'): #getcount
+    if (command == b'\x08'): #error
+    if (command == b'\x09'): #got the flag
+        
+    
+
+
 
 
 
